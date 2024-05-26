e67e4193bf991b7f4280772d60a8fd5651afb4ee
mohammadreza <shahbaziweb@gmail.com>
2024-05-26 15:59:30 +0330

    minor updates

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,17 +1,24 @@
 import { httpService } from "@services/api/httpService";
 
-export const getMovies = () => {
+const getMovies = () => {
   return httpService.get('/movies');
 }
 
-export const getMovie = (id: string) => {
+const getMovie = (id: string) => {
   return httpService.get(`/movies/${id}`);
 }
 
-export const addMovie = (movie: any) => {
+const addMovie = (movie: any) => {
   return httpService.post('/movies', movie);
 }
 
-export const editMovie = (movie: any) => {
+const editMovie = (movie: any) => {
   return httpService.put(`/movies/${movie.id}`, movie);
+}
+
+export const dataService = {
+  getMovies,
+  getMovie,
+  addMovie,
+  editMovie,
 }
