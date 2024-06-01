14c1980512a8aef2d083d4c494eda197e03ff2a3
mohammadreza <shahbaziweb@gmail.com>
2024-06-01 18:55:49 +0330

    minor updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const target = new URL('/auth/login', request.nextUrl.origin);
-  target.searchParams.append("returnUrl", request.nextUrl.pathname);
-  console.log(request)
-  return NextResponse.redirect(target);
+  // const {user} = globalStateService.get();
+  // if (authService.hasToken()) {
+  //   if (authService.hasPermission(user.permissions!)) {
+  //     return true
+  //   } else {
+  //     return NextResponse.redirect('/404');
+  //   }
+  // } else {
+  //   authService.logout();
+  //   const target = new URL('/auth/login', request.nextUrl.origin);
+  //   target.searchParams.append("returnUrl", request.nextUrl.pathname);
+  //   return NextResponse.redirect(target);
+  // }
 }
 
 export const config = {
-  matcher: ["/xx"],
+  matcher: ["/"],
 }
