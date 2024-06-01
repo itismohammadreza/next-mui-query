5501085534df7da62a37b4bee53ef4fed68e5179
mohammadreza <shahbaziweb@gmail.com>
2024-06-01 18:53:25 +0330

    create separate providers

--- src/app/_components/Providers.tsx
+++ src/app/_components/Providers.tsx
@@ -1,21 +1,21 @@
 'use client';
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@theme/theme";
 import { Loading } from "./Loading";
 import { Toast } from "./Toast";
+import { WithChildren } from "@models/common";
 import '@locales/i18n';
 
-export function Providers({ children }) {
-    const queryClient = new QueryClient();
+export const Providers = ({children}: WithChildren) => {
+  const queryClient = new QueryClient();
 
-    return (
-        <ThemeProvider>
-            <QueryClientProvider client={queryClient}>
-                <Loading />
-                <Toast />
-                {children}
-            </QueryClientProvider>
-        </ThemeProvider>
-    );
+  return (
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <Loading/>
+          <Toast/>
+          {children}
+        </QueryClientProvider>
+      </ThemeProvider>
+  );
 }
