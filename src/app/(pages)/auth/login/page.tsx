f3b9323327a8c72c488a5d411a514242e3e13328
mohammadreza <shahbaziweb@gmail.com>
2024-06-19 18:02:24 +0330

    minor updates

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -8,11 +8,12 @@ import { dataService } from "@services/api/dataService";
 import { useMutation } from "@tanstack/react-query";
 import { cookieService } from "@utils/coockieService";
 import { globalStateService } from "@services/globalStateService";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { User } from "@models/business";
 
 const Login = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const {isPending, mutateAsync: login} = useMutation({
     mutationFn: async (value: User) => {
@@ -30,7 +31,7 @@ const Login = () => {
   const onSubmit = async (value: User) => {
     const user = await login(value);
     if (user) {
-      router.push('/');
+      router.push(searchParams.get('return') ?? '/');
     }
   }
 
