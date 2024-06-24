f5a441f200e4f907a63e2940fedca03cee323f52
mohammadreza <shahbaziweb@gmail.com>
2024-06-24 12:50:01 +0330

    minor updates

--- src/app/(pages)/(main)/(home)/page.tsx
+++ src/app/(pages)/(main)/(home)/page.tsx
@@ -1,35 +1,14 @@
-"use client"
-import { Button, Typography } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import { dataService } from "@services/api/dataService";
-import { LoadingButton } from "@mui/lab";
 import { Page } from "@components/Page";
-import { useUser } from "@hooks/useUser";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Home'
+};
 
 const Home = () => {
-  const user = useUser();
-  const {isFetching, refetch} = useQuery({
-    queryKey: ["data"],
-    queryFn: dataService.getProducts,
-    enabled: false
-  });
-
   return (
-      <Page title="Home">
-        <Typography variant="h6" component="span"> Request: </Typography>
-        <LoadingButton loading={isFetching} onClick={() => refetch()}>
-          Call
-        </LoadingButton>
-        <br/>
-        {
-            user && (
-                <>
-                  <Typography variant="h6" component="span"> Logged In User: </Typography>
-                  {user.name} - {user.email}
-                  <Button color="error" onClick={() => dataService.logout()}> Logout </Button>
-                </>
-            )
-        }
+      <Page>
+        <h2>Home Page</h2>
       </Page>
   )
 }
